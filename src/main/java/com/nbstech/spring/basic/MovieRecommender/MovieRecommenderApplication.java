35d82bc779b977620757567f8e67a08e0c95766f
nguyennamcuong <cuong@gmail.com>
2022-12-17 06:35:00 +0700

    Init generated by Spring Initializer

---
+++ src/main/java/com/nbstech/spring/basic/MovieRecommender/MovieRecommenderApplication.java
@@ -0,0 +1,15 @@
+package com.nbstech.spring.basic.MovieRecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderApplication.class, args);
+		System.out.println("hello");
+		System.out.println("hello 2");
+	}
+
+}
---
+++ src/test/java/com/nbstech/spring/basic/MovieRecommender/MovieRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbstech.spring.basic.MovieRecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
